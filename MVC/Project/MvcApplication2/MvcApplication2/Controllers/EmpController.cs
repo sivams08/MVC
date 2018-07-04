b696eb5f3c56d19ec6db2393b95fedd68284cf18
sivams08 <39452037+sivams08@users.noreply.github.com>
2018-07-04 16:42:12 +0530

    Add files via upload

---
+++ MVC/Project/MvcApplication2/MvcApplication2/Controllers/EmpController.cs
@@ -0,0 +1,88 @@
+﻿using MvcApplication2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class EmpController : Controller
+    {
+        //
+        // GET: /Emp/
+        static List<Employee> EData = new List<Employee>();
+        public ActionResult EmployeeDetail()
+        {
+            return View(EData);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee E)
+        {
+            EData.Add(E);
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            foreach (Employee E1 in EData)
+            {
+                if (E1.EmpId == id)
+                    return View(E1);
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee E)
+        {
+            foreach (Employee E1 in EData)
+            {
+                if (E1.EmpId == E.EmpId)
+                {
+                    E1.EmpName = E.EmpName;
+                    E1.Gender = E.Gender;
+                }
+            }
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            foreach (Employee obj in EData)
+            {
+                if (obj.EmpId == id)
+                {
+                    return View(obj);
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            foreach (Employee E1 in EData)
+            {
+                if (E1.EmpId == id)
+                    return View(E1);
+            }
+            return View();
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Deleteconfirmed(int id)
+        {
+            foreach (Employee E1 in EData)
+            {
+                if (E1.EmpId == id)
+                {
+                    EData.Remove(E1);
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC/Project/MvcApplication2/MvcApplication2/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Gender { get; set; }
+    }
+}
